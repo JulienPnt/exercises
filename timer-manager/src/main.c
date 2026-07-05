1536cfc3bc8ce92415b003493ff2a765487f6c64
Julien Pnt <julien99pornet@gmail.com>
2026-07-05 15:34:53 +0200

    [timer-manager] ADD: setup area for sort algo

--- timer-manager/src/main.c
+++ timer-manager/src/main.c
@@ -1,10 +1,38 @@
-#include <assert.h>
+#include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /* #ifdef UNIT_TEST*/
 #include "unit_test.h"
 
 /* #endif */
+
+uint8_t cast_array_to_str(const uint8_t *array, const size_t szx, char **str) {
+  if (array == NULL) {
+    return 1;
+  }
+  if (szx == 0) {
+    return 2;
+  }
+  size_t i = 0;
+  *str = malloc(szx * 4 * sizeof(char));
+  for (i = 0; i < szx; i++) {
+    sprintf(*str + i * 4, "%.3u;", array[i]);
+  }
+  return 0;
+}
+
+uint8_t setup_random_array(uint8_t *array, const size_t size,
+                           const uint8_t max) {
+  size_t i = 0;
+  uint8_t rd_value = 0;
+  for (i = 0; i < size; i++) {
+    rd_value = rand() % (max + 1);
+    array[i] = rd_value;
+  }
+  return 0;
+}
 
 #define BUFFER_SIZE 10
 
@@ -13,6 +41,15 @@ int main() {
 
   timer_ring_buffer_unit_test();
   /* #endif */
+
+  uint8_t array[BUFFER_SIZE] = {0};
+  setup_random_array(array, BUFFER_SIZE, 20);
+  char *str = NULL;
+  cast_array_to_str(array, BUFFER_SIZE, &str);
+
+  printf("array = %s\n", str);
+  free(str);
+  str = NULL;
 
   return 0;
 }
