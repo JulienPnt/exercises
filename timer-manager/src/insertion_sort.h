0cab3ddb0068a371b83acdf28742bdc1fd24ccb7
Julien Pnt <julien99pornet@gmail.com>
2026-07-05 22:19:33 +0200

    [timer-manager] ADD: comments for sort algorythm

--- timer-manager/src/insertion_sort.h
+++ timer-manager/src/insertion_sort.h
@@ -5,6 +5,17 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+/*
+ * Testing purpose
+ */
 uint8_t insertion_sort(uint8_t *array, const size_t size);
+
+/* Insertion sort algorythm
+ * @rg const timer's array index reference where to start the sort
+ * @rg const size_t size number of index reference to sort
+ * @status:
+ *    - 0 in case of sucess
+ *    - never returns status != 0
+ */
 uint8_t insertion_sort_rtime(const rtimer_t *array, const size_t size);
 #endif
